158b0f8ed576925cde26673c4529fb9017f6d366
Eric Muganga <105128548+eric-muganga@users.noreply.github.com>
2025-02-16 10:51:50 +0100

    initial commit

---
+++ src/main/java/com/eric/ecommerce_product_service/EcommerceProductServiceApplication.java
@@ -0,0 +1,13 @@
+package com.eric.ecommerce_product_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceProductServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceProductServiceApplication.class, args);
+	}
+
+}
