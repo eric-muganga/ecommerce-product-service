d14ec3060fa40b4528e903b98a7cbcf8c6e6cc03
Eric Muganga <105128548+eric-muganga@users.noreply.github.com>
2025-04-10 23:55:50 +0200

    feat(entity): Create Review entity with relationship to Product - Defined `Review` entity with fields: id, userId, comment, rating, createdAt - Added `@ManyToOne` relationship with Product entity via `product` field - Annotated `rating` field as non-nullable and limited to 1-5 scale (validated in service layer)

---
+++ src/main/java/com/eric/ecommerce_product_service/entities/Review.java
@@ -0,0 +1,36 @@
+package com.eric.ecommerce_product_service.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "reviews")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Review {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long userId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    private String comment;
+
+    @Column(nullable = false)
+    private int rating; // 1–5
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+}
