f9e01d5d1d94897ae3156dd8afb27b2c345532eb
Eric Muganga <105128548+eric-muganga@users.noreply.github.com>
2025-04-13 19:47:58 +0200

    feat(entity): Introduce Category entity for product classification - Added fields: id, name - Future linkage with Product entity for filtering/sorting

---
+++ src/main/java/com/eric/ecommerce_product_service/entities/Category.java
@@ -0,0 +1,27 @@
+package com.eric.ecommerce_product_service.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+
+@Entity
+@Table(name = "categories")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    private String description;
+
+    @OneToMany(mappedBy = "category")
+    private List<Product> products;
+}
