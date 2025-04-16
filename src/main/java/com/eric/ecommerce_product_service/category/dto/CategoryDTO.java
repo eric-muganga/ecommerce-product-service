ec78c7a6d79dad6e67caac30a8a100f3d5e093b9
Eric Muganga <105128548+eric-muganga@users.noreply.github.com>
2025-04-16 17:24:20 +0200

    feat(dto): create CategoryDTO to represent category data
    - Useful for returning or referencing category objects

---
+++ src/main/java/com/eric/ecommerce_product_service/category/dto/CategoryDTO.java
@@ -0,0 +1,10 @@
+package com.eric.ecommerce_product_service.category.dto;
+
+import lombok.Data;
+
+@Data
+public class CategoryDTO {
+    private Long id;
+    private String name;
+    private String description;
+}
